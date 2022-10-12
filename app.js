f4277923f24a012833e1b00e1709ca34ce1f405b
PirrachoDev <pirracho_handsome@hotmail.com>
2022-10-11 22:58:11 -0600

    try to upload start script to heroku

--- app.js
+++ app.js
@@ -165,5 +165,5 @@ app.use((err, req, res, next) => {
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
-    console.log("SERVING PORT " + port);
+    console.log("SERVING PORT " + port); //just to update
 });
