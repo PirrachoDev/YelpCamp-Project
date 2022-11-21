e09eb67721cf7bb77b9d33c6aca6faf3343a4c51
Jose Argueta <92650152+PirrachoDev@users.noreply.github.com>
2022-11-21 22:08:22 +0000

    worked on frontend pagination

--- app.js
+++ app.js
@@ -17,8 +17,8 @@ const User = require('./models/user');
 const ExpressError = require('./utils/ExpressError');
 const mongoSanitize = require('express-mongo-sanitize');
 const helmet = require('helmet');
-//const dbUrl = /*process.env.DB_URL || */'mongodb://localhost:27017/yelp-camp'; // VSCode
-const dbUrl = process.env['DB_URL'] // || 'mongodb://localhost:27017/yelp-camp'; // Replit
+const dbUrl = process.env.DB_URL || 'mongodb://localhost:27017/yelp-camp'; // VSCode
+//const dbUrl = process.env['DB_URL'] // || 'mongodb://localhost:27017/yelp-camp'; // Replit
 
 const campgroundsRoutes = require('./routes/campgrounds');
 const reviewsRoutes = require('./routes/reviews');
